83ca97be7dd2127fff939614844aae67aad22b87
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-01-10 00:37:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-installdir=/home/pi/scripts/RPiUS
+installdir=/home/pi/scripts/RPiUSB
 
 [[ -d $installdir ]] || mkdir -p $installdir
 
-for file in $(curl -sS https://raw.githubusercontent.com/darton/RPiUS/master/files.txt); do
-   curl -sS https://raw.githubusercontent.com/darton/RPiUS/master/$file > $installdir/$file
+for file in $(curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/files.txt); do
+   curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/$file > $installdir/$file
 done
 
 sudo mv $installdir/wpa_supplicant.conf /etc/wpa_supplicant/
@@ -21,14 +21,14 @@ echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dtoverlay=gpio-shutdown,gpio_pin=4" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
 
-sudo mkdir /mnt/rpius
-sudo dd bs=1M if=/dev/zero of=/rpius.bin count=2048
-sudo mkdosfs /rpius.bin -F 32 -I
-echo "/rpius.bin /mnt/rpius vfat users,umask=000 0 0" | sudo tee -a /etc/fstab
+sudo mkdir /mnt/rpiusb
+sudo dd bs=1M if=/dev/zero of=/rpiusb.bin count=2048
+sudo mkdosfs /rpiusb.bin -F 32 -I
+echo "/rpiusb.bin /mnt/rpiusb vfat users,umask=000 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
-sudo ln -s /mnt/rpius /home/pi/rpius
+sudo ln -s /mnt/rpiusb /home/pi/rpiusb
 sudo chmod 777 /mnt
-sudo chmod 777 /mnt/rpius
+sudo chmod 777 /mnt/rpiusb
 
 sudo apt-get remove --purge libreoffice* -y
 sudo apt-get purge wolfram-engine -y
@@ -36,8 +36,8 @@ sudo apt-get clean
 sudo apt-get autoremove -y
 sudo apt-get install lftp
 
-cat $installdir/cron |sudo tee /etc/cron.d/rpius
+cat $installdir/cron |sudo tee /etc/cron.d/rpiusb
 rm $installdir/cron
 
-echo 'sudo modprobe g_mass_storage file=/rpius.bin stall=0 removable=1 idVendor=0x0781 idProduct=0x5572 bcdDevice=0x011a iManufacturer=\"RPiUS\" iProduct=\"USB Storage\" iSerialNumber=\"1234567890\"5572 bcdDevice=0x011a iManufacturer=\"RPiUS\"' | sudo tee -a /etc/rc.local
+echo 'sudo modprobe g_mass_storage file=/rpiusb.bin stall=0 removable=1 idVendor=0x0781 idProduct=0x5572 bcdDevice=0x011a iManufacturer=\"RPiUSB\" iProduct=\"USB Storage\" iSerialNumber=\"1234567890\"5572 bcdDevice=0x011a iManufacturer=\"RPiUSB\"' | sudo tee -a /etc/rc.local
 
