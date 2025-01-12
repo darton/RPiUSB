e010795d86c3897b0e47253577fce4142c79dbf6
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-01-12 16:02:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 installdir=/home/pi/scripts/RPiUSB
+mntdir=/mnt/rpiusb
+is_direct_execution="no"
 
 echo "Do you want to install the RPiUSB software?"
 read -r -p "$1 [y/N] " response < /dev/tty
-if [[ $response =~ ^(yes|y|Y)$ ]]; then
+if [[ "$response" =~ ^(yes|y|Y)$ ]]; then
     echo "Greats ! The installation has started."
 else
     echo "OK. Exiting"
     exit
 fi
 
-sudo apt update -y && sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt install lftp -y
 
+if [[ "$0" != "bash" ]] ; then
+    is_direct_execution="yes"
+fi
 
-[[ -d $installdir ]] || mkdir -p $installdir
+[[ -d "$installdir" ]] || mkdir -p "$installdir"
 
-for file in $(curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/files.txt); do
-   curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/$file > $installdir/$file
-done
-
-#sudo mv $installdir/wpa_supplicant.conf /etc/wpa_supplicant/
-#sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
-#sudo wpa_cli -i wlan0 reconfigure
-#sudo wpa_cli -i wlan0 reconnect
-
-#echo "interface wlan0" | sudo tee -a /etc/dhcpcd.conf
-#echo "env ifwireless=1" | sudo tee -a /etc/dhcpcd.conf
-#echo "env wpa_supplicant_driver=wext,nl80211" | sudo tee -a /etc/dhcpcd.conf
+if [[ "$is_direct_execution" == "no" ]]; then
+    for file in $(curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/files.txt); do
+        echo curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/"$file" > "$installdir"/"$file"
+    done
+else
+    script_dir=$(dirname "$(realpath "$0")")
+    for file in $(cat "$script_dir"/files.txt); do
+        cp "$script_dir/$file"  "$installdir"
+    done
+fi
 
 echo "dtoverlay=dwc2" | sudo tee -a /boot/firmware/config.txt
 echo "dtoverlay=gpio-shutdown" | sudo tee -a /boot/firmware/config.txt
@@ -44,25 +44,29 @@ idProduct=0x5572 \
 bcdDevice=0x011a \
 iManufacturer="RPiUSB" \
 iProduct="USB Storage" \
-iSerialNumber="1234567890"' | sudo tee -a /etc/modprobe.d/g_mass_storage.conf
+iSerialNumber="1234567890"' | sudo tee /etc/modprobe.d/g_mass_storage.conf
 
 
-sudo mkdir /mnt/rpiusb
+sudo mkdir -p "$mntdir"
 sudo dd bs=1M if=/dev/zero of=/rpiusb.bin count=4K status=progress
 sudo mkdosfs /rpiusb.bin -F 32 -I
-echo "/rpiusb.bin /mnt/rpiusb vfat users,umask=000 0 0" | sudo tee -a /etc/fstab
+echo "/rpiusb.bin $mntdir vfat users,umask=000 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
-sudo ln -s /mnt/rpiusb /home/pi/rpiusb
+sudo ln -s "mntdir" "$installdir"
 sudo chmod 777 /mnt
-sudo chmod 777 /mnt/rpiusb
+sudo chmod 777 "$mntdir"
 
+echo "# RPiUSB cron jobs"  |sudo tee /etc/cron.d/rpiusb
+echo "* 6-22 * * 1-6 pi bash $installdir/rpiusb.sh > /dev/null 2>&1" |sudo tee -a /etc/cron.d/rpiusb
 
-cat $installdir/cron |sudo tee /etc/cron.d/rpiusb
-rm $installdir/cron
 chmod u+x $installdir/*.sh
 
 sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_boot_behaviour B1
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt install lftp -y
 
 echo ""
 echo "-------------------------------------"
@@ -75,7 +79,7 @@ echo ""
 read -r -p "$1 [y/N] " response < /dev/tty
 
 if [[ $response =~ ^(yes|y|Y)$ ]]; then
-    nano $installdir/rpiusb.sh
+    nano $installdir/.config
 fi
 
 echo "Reboot is necessary for proper RPiUSB operation."
