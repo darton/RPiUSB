703898c3c222c804e542823d794d2cd795ad1bd5
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-01-12 16:45:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ fi
 
 if [[ "$is_direct_execution" == "no" ]]; then
     for file in $(curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/files.txt); do
-        echo curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/"$file" > "$installdir"/"$file"
+        curl -sS https://raw.githubusercontent.com/darton/RPiUSB/master/"$file" > "$installdir"/"$file"
     done
 else
     script_dir=$(dirname "$(realpath "$0")")
